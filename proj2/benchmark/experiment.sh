73461a05efb86b4a7fa164549312bba181f57333
Stevie Xie <66903483+shiitavie@users.noreply.github.com>
2022-11-16 11:56:33 -0600

    fixed sbatch

--- proj2/benchmark/experiment.sh
+++ proj2/benchmark/experiment.sh
@@ -5,14 +5,14 @@
 #SBATCH --job-name=proj2-experiment
 #SBATCH --output=./slurm/out/%j.%N.stdout
 #SBATCH --error=./slurm/out/%j.%N.stderr
-#SBATCH --chdir=/home/zhiyuanxie/proj1-shiitavie/proj1/benchmark
+#SBATCH --chdir=/home/zhiyuanxie/project-2-shiitavie/proj2/benchmark
 #SBATCH --partition=debug 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2000
 #SBATCH --exclusive
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 
 python3 time_experiment.py
 python3 output_reader.py 
